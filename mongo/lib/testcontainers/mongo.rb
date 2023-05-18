79cdb6da3fd92f22aa0b805338240c291873317a
Pedro Piza <pedro92_0224@hotmail.com>
2023-05-18 00:57:17 -0500

    Add MongoDB container with tests, using mongo as gem client to connect to the mongo container

---
+++ mongo/lib/testcontainers/mongo.rb
@@ -0,0 +1,101 @@
+require_relative "mongo/version"
+require "testcontainers"
+
+module Testcontainers
+  # MongoContainer class is used to manage containers that runs a Mongo databese
+  #
+  # @attr_reader [String] port used by container
+  # @attr_reader [String] username used by the container
+  # @attr_reader [String] password used by the container
+  # @attr_reader [String] database used by the container
+  class MongoContainer < ::Testcontainers::DockerContainer
+    # Default port used by the container
+    MONGO_DEFAULT_PORT = 27017
+
+    # Default image used by the container
+    MONGO_DEFAULT_IMAGE = "mongo:latest"
+
+    MONGO_DEFAULT_USERNAME = "test"
+    MONGO_DEFAULT_PASSWORD = "test"
+    MONGO_DEFAULT_ROOT_USERNAME = "root"
+    MONGO_DEFAULT_DATABASE = "test"
+
+    attr_reader :port, :username, :password, :database
+
+    # Initializes a new instance of MongoContainer
+    #
+    # @param image [String] the image to use
+    # @param username [String] the username to use
+    # @param password [String] the password to use
+    # @param database [String] the database to use
+    # @param port [String] the port to use
+    # @param kwargs [Hash] the options to pass to the container. See {DockerContainer#initialize}
+    # @return [MongoContainer] a new instance of MongoContainer
+    def initialize(image = MONGO_DEFAULT_IMAGE, username: nil, password: nil, database: nil, port: nil, **kwargs)
+      super(image, **kwargs)
+      @port = port || ENV.fetch("MONGO_PORT", MONGO_DEFAULT_PORT)
+      @username = username || ENV.fetch("MONGO_USERNAME", MONGO_DEFAULT_USERNAME)
+      @password = password || ENV.fetch("MONGO_PASSWORD", MONGO_DEFAULT_PASSWORD)
+      @database = database || ENV.fetch("MONGO_DATABASE", MONGO_DEFAULT_DATABASE)
+    end
+
+    # Starts the container
+    #
+    # @return [MongoContainer] self
+    def start
+      with_exposed_ports(@port)
+      _configure
+      super
+    end
+
+    # Returns the database url (e.g. mongodb://user:password@host:port/database)
+    # @param protocol [String] the protocol to use in the string (default: "mongodb")
+    # @param database [String] the database to use in the string (default: @database)
+    # @param options [Hash] the options to use in the query string (default: {})
+    # @return [String] the database url
+    # @raise [ConnectionError] If the connection to the Docker daemon fails.
+    # @raise [ContainerNotStartedError] If the container has not been started.
+    def mongo_url(protocol: "mongodb", username: nil, password: nil, database: nil, options: {})
+      database ||= @database
+      username ||= @username
+      password ||= @password
+      query_string = options.empty? ? "" : "?#{URI.encode_www_form(options)}"
+
+      "#{protocol}://#{username}:#{password}@#{host}:#{mapped_port(port)}/#{database}#{query_string}"
+    end
+
+    # Sets the database to use
+    # @param database [String] the database to use
+    # @return [MongoContainer] self
+    def with_database(database)
+      @database = database
+      self
+    end
+
+    # Sets the username to use
+    #
+    # @param username [String] the password to use
+    # @return [MongoContainer] self
+    def with_username(username)
+      @password = password
+      self
+    end
+
+    # Sets the password to use
+    #
+    # @param password [String] the password to use
+    # @return [MongoContainer] self
+    def with_password(password)
+      @password = password
+      self
+    end
+
+    private
+
+    def _configure
+      add_env("MONGO_INITDB_DATABASE", @database)
+      add_env("MONGO_INITDB_ROOT_USERNAME", @username)
+      add_env("MONGO_INITDB_ROOT_PASSWORD", @password)
+    end
+  end
+end
---
+++ mongo/test/mongo_container_test.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class MongoContainerTest < TestcontainersTest
+  def before_all
+    super
+
+    @container = Testcontainers::MongoContainer.new
+    @container.start
+    @host = @container.host
+    @port = @container.first_mapped_port
+  end
+
+  def after_all
+    if @container&.exists?
+      @container&.stop if @container&.running?
+      @container&.remove
+    end
+
+    super
+  end
+
+  def test_it_returns_the_default_image
+    assert_equal "mongo:latest", @container.image
+  end
+
+  def test_it_supports_custom_image
+    container = Testcontainers::MongoContainer.new("mariadb:latest")
+    assert_equal "mariadb:latest", container.image
+  end
+
+  def test_it_returns_the_default_port
+    assert_equal 27017, @container.port
+  end
+
+  def test_it_is_configured_with_the_default_environment_variables
+    assert "test", @container.get_env("MONGO_DATABASE")
+    assert "test", @container.get_env("MONGO_USER")
+    assert "test", @container.get_env("MONGO_PASSWORD")
+  end
+
+  def test_it_has_the_default_port_mapped
+    assert @container.mapped_port(27017)
+  end
+
+  def test_it_supports_custom_port
+    container = Testcontainers::MongoContainer.new(port: 13306)
+    assert_equal 13306, container.port
+  end
+
+  def test_it_supports_custom_keyword_arguments
+    container = Testcontainers::MongoContainer.new(filesystem_binds: ["#{Dir.pwd}/custom/conf:/etc/mongo/mongod.conf:rw"])
+    assert_equal ["#{Dir.pwd}/custom/conf:/etc/mongo/mongod.conf:rw"], container.filesystem_binds
+  end
+
+  def test_it_returns_the_default_database_url
+    assert_equal "mongodb://test:test@#{@host}:#{@port}/test", @container.mongo_url
+  end
+
+  def test_it_returns_the_database_url_with_custom_database
+    assert_equal "mongodb://test:test@#{@host}:#{@port}/foo", @container.mongo_url(database: "foo")
+  end
+
+  def test_it_returns_the_database_url_with_custom_protocol
+    assert_equal "mongodb2://test:test@#{@host}:#{@port}/test", @container.mongo_url(protocol: "mongodb2")
+  end
+
+  def test_it_returns_the_database_url_with_custom_username
+    assert_equal "mongodb://foo:test@#{@host}:#{@port}/test", @container.mongo_url(username: "foo")
+  end
+
+  def test_it_returns_the_database_url_with_custom_password
+    assert_equal "mongodb://test:bar@#{@host}:#{@port}/test", @container.mongo_url(password: "bar")
+  end
+
+  def test_it_is_reachable
+    @container.wait_for_logs(/Waiting for connections/)
+
+    client = Mongo::Client.new("mongodb://test:test@127.0.0.1:#{@port}/test", auth_source: "admin")
+    client[:artists].insert_one({name: "FKA Twigs"})
+
+    assert_equal 1, client[:artists].find(name: "FKA Twigs").count_documents
+  end
+end
