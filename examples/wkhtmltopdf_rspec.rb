3fed862a4b34416f97176f1773277bbf3f67c0e6
Luis Porras <lporras16@gmail.com>
2023-05-30 10:59:08 -0500

    adding example of usage with wkhtmltopdf command line tool

---
+++ examples/wkhtmltopdf_rspec.rb
@@ -0,0 +1,39 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'rspec'
+  gem 'wkhtmltopdf-binary'
+  gem 'testcontainers-core', path: '../core', require: 'testcontainers'
+end
+
+require 'rspec'
+require 'rspec/autorun'
+
+RSpec.configure do |config|
+end
+
+describe 'Wkhtmltopdf Example' do
+  let(:url) { 'https://google.com' }
+  let(:pdfs_path) { "#{__dir__}/pdfs:/pdfs:rw" }
+  let(:command) { [url, file_name] }
+  let(:file_name) { '/pdfs/google.pdf' }
+  let(:container) do
+    Testcontainers::DockerContainer.new('surnet/alpine-wkhtmltopdf:3.17.0-0.12.6-small')
+  end
+
+  before do
+    container.with_filesystem_binds([pdfs_path])
+             .with_command(command)
+             .start
+  end
+
+  after do
+    container&.stop
+    container&.remove
+  end
+
+  it 'generates a PDF page from URL' do
+    expect(File.file?(__dir__ + file_name)).to be_truthy
+  end
+end
