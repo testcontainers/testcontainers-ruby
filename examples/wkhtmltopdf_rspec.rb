c67e8bc0193adc5474239a62cea8a0acafe8f425
Luis Porras <lporras16@gmail.com>
2023-05-30 16:17:44 -0500

    standard fixes

--- examples/wkhtmltopdf_rspec.rb
+++ examples/wkhtmltopdf_rspec.rb
@@ -1,32 +1,32 @@
-require 'bundler/inline'
+require "bundler/inline"
 
 gemfile do
-  source 'https://rubygems.org'
-  gem 'rspec'
-  gem 'wkhtmltopdf-binary'
-  gem 'testcontainers-core', path: '../core', require: 'testcontainers'
+  source "https://rubygems.org"
+  gem "rspec"
+  gem "wkhtmltopdf-binary"
+  gem "testcontainers-core", path: "../core", require: "testcontainers"
 end
 
-require 'rspec'
-require 'rspec/autorun'
+require "rspec"
+require "rspec/autorun"
 
 RSpec.configure do |config|
 end
 
-describe 'Wkhtmltopdf Example' do
-  let(:url) { 'https://google.com' }
+describe "Wkhtmltopdf Example" do
+  let(:url) { "https://google.com" }
   let(:pdfs_path) { "#{__dir__}/pdfs:/pdfs:rw" }
   let(:command) { [url, file_name] }
-  let(:file_name) { '/pdfs/google.pdf' }
+  let(:file_name) { "/pdfs/google.pdf" }
   let(:container) do
-    Testcontainers::DockerContainer.new('surnet/alpine-wkhtmltopdf:3.17.0-0.12.6-small')
+    Testcontainers::DockerContainer.new("surnet/alpine-wkhtmltopdf:3.17.0-0.12.6-small")
   end
   let(:file_path) { __dir__ + file_name }
 
   before do
     container.with_filesystem_binds([pdfs_path])
-             .with_command(command)
-             .start
+      .with_command(command)
+      .start
   end
 
   after do
@@ -34,7 +34,7 @@ describe 'Wkhtmltopdf Example' do
     container&.remove
   end
 
-  it 'generates a PDF page from URL' do
+  it "generates a PDF page from URL" do
     expect(File).to exist(file_path)
   end
 end
