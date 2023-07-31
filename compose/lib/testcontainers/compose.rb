d088c1c9f86ef1f1e250ec6b1467ef705ab37838
malboro-eng <jpizarodrugiez@gmail.com>
2023-07-31 11:38:38 -0500

    add compose enviroment with all functions

--- compose/lib/testcontainers/compose.rb
+++ compose/lib/testcontainers/compose.rb
@@ -2,6 +2,9 @@ require_relative "compose/version"
 require "testcontainers"
 require "open3"
 require "pry"
+require "net/http"
+require "json"
+require "uri"
 module Testcontainers
   # ComposeContainer class is used to manage a large number of containers in a synchronous environment
   #
@@ -10,10 +13,9 @@ module Testcontainers
   # @attr_accesor [Boolean] pull used by the container
   # @attr_accesor [Boolean] build used by the container
   # @attr_accesor [List] services used by the container
-  class ComposeContainer < ::Testcontainers::DockerContainer
+  class ComposeContainer
 
     #Default image used by the container
-    DOCKER_COMPOSE_IMAGE = "docker/compose:latest"
 
     attr_accessor :filepath, :compose_file_name, :pull, :build, :env_file, :services
 
@@ -26,70 +28,111 @@ module Testcontainers
     # @param build [Boolean] is the option for decide if there have to use a build command for the images used for the containers
     # @param env_file [String] is the name of the envieroment configuration
     # @param services [List] are the names of the services that gonna use in the images of the containers 
-    def initialize( image: DOCKER_COMPOSE_IMAGE, filepath: nil, compose_file_name: ["docker-compose.yml"], pull: false, build: false, env_file: nil , services: nil, **kwargs)
-      super(image, **kwargs)
+    def initialize(filepath: ".", compose_file_name: ["docker-compose.yml"], pull: false, build: false, env_file: nil , services: nil, **kwargs)
       @filepath = filepath
       @compose_file_names = compose_file_name
       @pull = pull
       @build = build
       @services = services
-      @env_file = env_file 
+      @env_file = env_file
     end
 
     # Specify the names of the all names of the configuration files
     # @return [docker_compose_cmd] 
     def with_command
-      docker_compose_cmd = ['docker-compose']
+      docker_compose_cmd = ['docker compose']
       @compose_file_names.each do |file|
         docker_compose_cmd += ["-f  #{file}"]
       end
       if env_file.nil? == false
-        docker_compose_cmd += "--env-file #{@env_file}"
+        docker_compose_cmd.push("--env-file #{@env_file}")
       end
-      docker_compose_cmd
+      docker_compose_cmd.join(" ")
     end
 
     # Generete the commands for the containers add the sentences of the function of with_command
-    def create
-     binding.pry
+    def start
       if @pull
-        pull_cmd = "#{self.with_command()} pull"
-        self.call_command(pull_cmd)
+        pull_cmd = "#{with_command()} pull"
+        Open3.capture2(pull_cmd, chdir: @filepath)
       end
 
-      up_cmd =   " #{self.with_command()} up -d"
+      up_cmd =  " #{with_command()} up -d"
       if @build
-        up.cmd.push('--build')
+        up_cmd.concat(' --build')
       end
 
-      if @services.nil? == false
-        up_cmd.concat(@services)
+      if @services
+        up_cmd.concat(@services.join(" "))
       end
 
-      self.call_command(cmd: up_cmd)
+      if @build
+      binding.pry
+      end
+
+      Open3.capture2(up_cmd, chdir: @filepath)
     end
 
     # Generate the command for stop the containers
     def stop
-      down_cmd = self.with_command() + ['down', '-v']
-      self.call_command(cmd: down_cmd)
+      down_cmd = with_command().concat(' down -v')
+      #call_command(cmd: down_cmd, option: 1)
+      Open3.capture2(down_cmd, chdir: @filepath)
     end
 
-
     def logs
-      cmd = self.with_command + ["logs"]
-      stdout, stderr , status = Open3.capture3(*cmd, chdir: ".")
-      puts stderr
-      return stdout, stderr, status
+      command_logs = with_command.concat(" logs")
+      Open3.capture3(command_logs, chdir: @filepath)
+    end
+
+    def run_in_container(service_name: nil, command: nil )
+      command_exec = with_command.concat(" exec -T #{service_name} #{command}")
+      Open3.capture2(command_exec, chdir: @filepath)
+    end
+
+    def port_process(service: nil, port: 0)
+      process_information(service: service, port: port)[0]
+    end
+
+    def host_process(service: nil, port: 0)
+      process_information(service: service, port: port)[1]
+    end
+
+    def process_information(service:nil , port: 0)
+      command_port = with_command().concat(" port #{service} #{port}")
+    # stdout2, stderr2,status2 = call_command(cmd: command_port, option: 3)
+    # stdout2.split(":"), stderr2, status2
+     #stdout_splitted = stdout.split(":")
+     #stdout_splitted, stderr, status
+      stdout, stderr, status = Open3.capture3(command_port, chdir: @filepath)
+      host, port = stdout.strip.split(':')
+      [host, port, stderr, status]
     end
 
     # Execute the commands with multiprocess througt the library Open3
-    # @param [List] the commands to be executed   
-    def call_command(cmd: nil, filepath: ".")
-     stdout = Open3.capture2e(*cmd, chdir: filepath)
-     p "este es el estado #{stdout}"
+    # @param [List] the commands to be executed
+  ##def call_command(cmd: nil, option: 1)
+  ## case option
+  ## when 1
+  ##   stdout, status = Open3.capture2(cmd, chdir: @filepath)
+  ##   return stdout, status
+  ## when 2
+  ##   stdout2, stderr2 = Open3.popen3(cmd, chdir: @filepath)
+  ##   return stdout2, stderr2
+  ## when 3
+  ##   stdout3, stderr3 = Open3.capture3(cmd, chdir: @filepath)
+  ##   return stdout3,stderr3
+  ## else
+  ##   puts "Not command in the  code"
+  ## end
+  ##end
+
+    def wait_for_request(url: nil)
+      url =URI.parse(url) 
+      http = Net::HTTP.new(url.host, url.port)
+
+      request = Net::HTTP::Get.new(url)
+      http.request(request)
     end
-
-
   end
 end
--- compose/test/compose_container_test.rb
+++ compose/test/compose_container_test.rb
@@ -1,41 +1,114 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "socket"
 
+class Open3Stub
+  def initialize(expected_up_cmd, expected_filepath)
+    @expected_up_cmd = expected_up_cmd
+    @expected_filepath = expected_filepath
+  end
+
+  def capture2(up_cmd, options)
+    # Check if the captured arguments match the expected ones
+    assert_equal @expected_up_cmd, up_cmd
+    assert_equal({ chdir: @expected_filepath }, options)
+
+    # Return a dummy result (can be nil or any other value you need for testing)
+    [nil, nil]
+  end
+end
 
 class ComposeContainerTest < TestcontainersTest
 
-  TEST_PATH = Dir.getwd
+  TEST_PATH = Dir.getwd.concat("/test")
   def before_all
     super
 
     @container = Testcontainers::ComposeContainer.new(filepath: TEST_PATH)
     @container.start
-    @container.create
-    @host = @container.host
-    @port = @container.first_mapped_port
-
-    
-    @container.logs
-    binding.pry
   end
 
   def after_all
-    if @container&.exists?
-      @container&.stop if @container&.running?
-      @container&.remove
+    #if @container&.exists?
+    #  @container&.stop if @container&.running?
+    #  @container&.remove
+    #end
+
+    #super
+    @container.stop
+  end
+
+  def test_information_spawn
+    host = @container.host_process(service: "hub", port: 4444)
+    port = @container.port_process(service: "hub", port: 4444)
+
+    assert "0.0.0.0", host
+    assert 4444, port
+  end
+
+
+  def test_can_pull_before_build_in_spawn
+    container = Testcontainers::ComposeContainer.new(filepath: TEST_PATH, pull: true)
+    host = container.host_process(service: "hub", port: 4444)
+    port = container.port_process(service: "hub", port: 4444)
+
+    assert "0.0.0.0", host
+    assert 4444, port
+  end
+  
+  def test_can_build_images_before_spawning_service_via_compose
+    container = Testcontainers::ComposeContainer.new(filepath: TEST_PATH, build: true)
+    mock = Minitest::Mock.new
+    mock.expect(:capture2, nil, [" docker compose -f  docker-compose.yml up -d --build", {chdir: TEST_PATH}])
+
+    Open3.stub :capture2, proc { |cmd, opts| mock.capture2(cmd, opts) } do
+      assert container.build
+      container.start
+
     end
-
-    super
+    mock.verify
   end
 
-  def test_it_returns_the_default_image
-    assert_equal "docker/compose:latest", @container.image
+  def  test_with_specific_services
+    services = ["hub","firefox","chrome"]
+    container = Testcontainers::ComposeContainer.new(filepath: TEST_PATH, services: services)
+    container.start
+
+    assert_includes container.services, "hub"
+    assert_includes container.services, "firefox"
+    assert_includes container.services, "chrome"
   end
 
-  def test_it_supports_custom_capabilities_to_build_image
-    container = Testcontainers::ComposeContainer.new(image: "docker/compose")
-    assert_equal "docker/compose", container.image
+  def test_with_specific_compose_files
+    compose_file_name = ["docker-compose.yml","docker-compose2.yml"]
+    container = Testcontainers::ComposeContainer.new(filepath: TEST_PATH, compose_file_name: compose_file_name)
+
+    container.start
+
+    host = container.host_process(service: "hub", port: 4444)
+    port = container.port_process(service: "hub", port: 4444)
+
+    assert "0.0.0.0", host
+    assert 4444, port
+
   end
 
+  def  test_logs_for_process
+    container = Testcontainers::ComposeContainer.new(filepath: TEST_PATH)
+    host_name = Socket.gethostname
+    url = "http://#{host_name}:4444/ui"
+    container.wait_for_request(url: url)
+    stdout, stderr = container.logs
+    assert stdout
+  end
+
+  def test_can_pass_env_params
+    compose_file_name = ["docker-compose3.yml"] 
+    container = Testcontainers::ComposeContainer.new(filepath: TEST_PATH, compose_file_name: compose_file_name, env_file: ".env.test")
+    container.start
+    stdout, stderr = container.run_in_container(service_name: "alpine", command: "printenv TEST_ASSERT_KEY")
+    assert "successful test", stdout
+    container.stop
+  end
 end
